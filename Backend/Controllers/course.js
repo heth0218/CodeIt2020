8d53ce4fa639ce853a681628b1c989a034e17169
Jash271 <shahjash271@gmail.com>
2020-09-26 02:20:39 +0530

    Courses crud completed ✌

--- Backend/Controllers/course.js
+++ Backend/Controllers/course.js
@@ -30,7 +30,43 @@ exports.CreateCourse = async (req, res, next) => {
 
 exports.UpdateCourse = async (req, res, next) => {
   try {
+    const course = await Course.findByIdAndUpdate(
+      req.params.course_id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!course) {
+      return res.status(400).json({ msg: 'No such Course exists' });
+    }
+    return res.status(200).json({ success: true, data: course });
   } catch (error) {
     return res.status(500).json({ msg: Server_Error });
+  }
+};
+
+exports.DeleteCourse = async (req, res, next) => {
+  try {
+    const course = await Course.findByIdAndDelete(req.params.course_id);
+    if (!course) {
+      return res.status(400).json({ msg: 'No such Course exists' });
+    }
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+exports.OneCourse = async (req, res, next) => {
+  try {
+    const course = await Course.findById(req.params.course_id);
+    if (!course) {
+      return res.status(400).json({ msg: 'No such Course exists' });
+    }
+    return res.status(200).json({ success: true, data: course });
+  } catch (error) {
+    return res.status(500).json(error);
   }
 };
--- Backend/routes/Courses.js
+++ Backend/routes/Courses.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { admin_access, GetUser } = require('../middleware/auth');
 const { All_Courses_Cache } = require('../middleware/Courses_Cache_Middleware');
-const { AllCourses, CreateCourse } = require('../Controllers/course');
+const {
+  AllCourses,
+  CreateCourse,
+  UpdateCourse,
+  DeleteCourse,
+  OneCourse,
+} = require('../Controllers/course');
 
 router.route('/all_courses').get(All_Courses_Cache, AllCourses);
 router.route('/create_course').post(GetUser, CreateCourse);
+router.route('/update_course/:course_id').put(admin_access, UpdateCourse);
+router.route('/delete_course/:course_id').delete(admin_access, DeleteCourse);
+router.route('/one_course/:course_id').get(OneCourse);
 
 module.exports = router;
