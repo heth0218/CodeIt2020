05d216d81297f9a5e8fa1c241cd2dadf92ff9bae
Soham Dave <dave.soham2000@gmail.com>
2020-09-26 14:22:28 +0530

    Edited Video Model

--- Backend/Controllers/videos.js
+++ Backend/Controllers/videos.js
@@ -1,58 +1,59 @@
 const Video = require('../models/CourseVideo');
 const DB = require('../config/db').getDB;
 
-exports.addVideo = async (req, res, next) => {
-  try {
-    req.body.Publisher = req.user._id;
-    req.body.course = req.params.course_id;
-    const video = await Video.create(req.body);
-    return res.status(201).json({
-      msg: 'Video Added',
-      data: video,
-    });
-  } catch (e) {
-    res.status(500).json({ msg: 'Server error ' + e });
-  }
-};
-
-exports.getVideoAll = async (req, res, next) => {
-  try {
-    const courseName = req.body.cname;
-    Video.find({ CourseName: courseName })
-      .then((data) => {
-        if (data.length >= 1) {
-          res.status(200).json({
-            msg: 'Fetched all videos for ' + courseName,
-            data: data,
-          });
-        } else {
-          res.status(404).json({
-            msg: 'No Item found',
-          });
-        }
-      })
-      .catch((e) => {
-        res.status(500).json({ msg: 'Server error ' + e });
-      });
-  } catch (e) {
-    res.status(500).json({ msg: 'Server error ' + e });
-  }
-};
-
-exports.getOneVideo = async (req, res, next) => {
-  try {
-    const VideoTitle = req.body.VideoTitle;
-    Video.findOne({ Title: VideoTitle })
-      .then((data) => {
-        res.status(200).json({
-          msg: 'Fetched video for title - ' + VideoTitle,
-          data: data,
+exports.addVideo = async(req, res, next) => {
+    try {
+        req.body.Publisher = req.user._id;
+        req.body.course = req.params.course_id;
+        const video = await Video.create(req.body);
+        return res.status(201).json({
+            msg: 'Video Added',
+            data: video,
         });
-      })
-      .catch((e) => {
+    } catch (e) {
         res.status(500).json({ msg: 'Server error ' + e });
-      });
-  } catch (e) {
-    res.status(500).json({ msg: 'Server error ' + e });
-  }
+    }
+};
+
+exports.getVideoAll = async(req, res, next) => {
+    try {
+        req.body.course = req.params.course_id;
+        const courseName = req.body.course;
+        Video.find({ course: courseName })
+            .then((data) => {
+                if (data.length >= 1) {
+                    res.status(200).json({
+                        msg: 'Fetched all videos',
+                        data: data,
+                    });
+                } else {
+                    res.status(404).json({
+                        msg: 'No Item found',
+                    });
+                }
+            })
+            .catch((e) => {
+                res.status(500).json({ msg: 'Server error ' + e });
+            });
+    } catch (e) {
+        res.status(500).json({ msg: 'Server error ' + e });
+    }
+};
+
+exports.getOneVideo = async(req, res, next) => {
+    try {
+        const VideoTitle = req.body.VideoTitle;
+        Video.findOne({ Title: VideoTitle })
+            .then((data) => {
+                res.status(200).json({
+                    msg: 'Fetched video for title - ' + VideoTitle,
+                    data: data,
+                });
+            })
+            .catch((e) => {
+                res.status(500).json({ msg: 'Server error ' + e });
+            });
+    } catch (e) {
+        res.status(500).json({ msg: 'Server error ' + e });
+    }
 };
--- Backend/routes/video.js
+++ Backend/routes/video.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { admin_access, GetUser } = require('../middleware/auth');
 const { All_Courses_Cache } = require('../middleware/Courses_Cache_Middleware');
-const { addVideo } = require('../Controllers/videos');
+const { addVideo, getVideoAll, getOneVideo } = require('../Controllers/videos');
 
 router.route('/add_video/:course_id').post(admin_access, addVideo);
-
+router.route('/get_video_all/:course_id').get(admin_access, getVideoAll);
+router.route('/get_video_byTitle').post(admin_access, getOneVideo);
 module.exports = router;
